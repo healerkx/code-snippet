31c7f0e5cbf597bc3bc6ac49101d32fe7132dc06
zmyu <zmyu@yahoo-inc.com>
2014-02-25 15:55:55 +0800

    Add new files

--- slugfest/gpass/gpass.go
+++ slugfest/gpass/gpass.go
@@ -4,11 +4,10 @@ import (
     "fmt"
     "time"
 
-	"html/template"
+    "html/template"
     "net/http"
     "appengine"
     "appengine/datastore"
-    //"appengine/log"
     "appengine/user"
 )
 
@@ -19,77 +18,57 @@ type Entry struct {
     Date 		time.Time
 }
 
-
-
-
 func init() {
-    http.HandleFunc("/", welcome)
-    http.HandleFunc("/list", list)
-    http.HandleFunc("/addnew", addnew)
-    http.HandleFunc("/add", add)
+    http.HandleFunc("/h", handler)
+    http.HandleFunc("/add", add)    
 }
 
-func welcome(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
+
     c := appengine.NewContext(r)
-    u, err := user.CurrentOAuth(c, "")
-    url, err := user.LoginURL(c, "list")
-    if err != nil {
-        fmt.Fprintf(w, "<a href='%s'>%s</a>", url, "Login")
-        return
-    }
-    if !u.Admin {
-        // http.Error(w, "Admin login only", http.StatusUnauthorized)
-        fmt.Fprintf(w, "<a href='%s'>%s</a>", url, "Login")
-        return
-    }
-    fmt.Fprintf(w, `Welcome, admin user %s!`, u)
-}
+    u := user.Current(c)
 
-func list(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u, _ := user.CurrentOAuth(c, "")
-
-    c.Infof("Current Account: %v", u.Email)
-    if u.Email != "healer.kx.yu@gmail.com" {
-        url, _ := user.LoginURL(c, "list")
-        http.Redirect(w, r, url, http.StatusMovedPermanently)
-        // fmt.Fprintf(w, "<a href='%s'>%s</a>", url, "Login")
+    if u == nil || u.Email != "healer.kx.yu@gmail.com" {
+        loginUrl, _ := user.LoginURL(c, "h")
+        c.Infof("User =: %v", u)
+        http.Redirect(w, r, loginUrl, http.StatusMovedPermanently)
         return
     }
 
-    t := template.New("listall")
-    t, err := t.ParseFiles("templates/listall.html")
+    t := template.New("gpass template")
+    t, err := t.ParseFiles("templates/gpass.html")
+
     if err != nil {
-        fmt.Fprint(w, err)
+        fmt.Fprint(w, "Error: %v", err)
     }
-    
-    err = t.ExecuteTemplate(w, "listall.html", u)
-    if err != nil {
-        fmt.Fprint(w, err)
-    }}
 
+    logoutUrl, _ := user.LogoutURL(c, "/")
 
+    query := datastore.NewQuery("Entry")
+    var entries []Entry
+    _, qe := query.GetAll(c, &entries)
 
-func addnew(w http.ResponseWriter, r *http.Request) {
-	t := template.New("addnew")
-	t, err := t.ParseFiles("templates/addnew.html")
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
+    if qe == nil {
+        vars := map[string]interface{} {
+            "email": u.Email,
+            "items": entries,
+            "logoutUrl": logoutUrl,
+        }
 
-	err = t.ExecuteTemplate(w, "addnew.html", nil)
-	if err != nil {
-		fmt.Fprint(w, err)
-	}
+        err = t.ExecuteTemplate(w, "gpass.html", vars)
+        if err != nil {
+            fmt.Fprint(w, "Error: %v", err)
+        }
+    }
 }
 
 func add(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     e := Entry{
-        Name:   r.FormValue("name"),
-        Value:  r.FormValue("value"),
+        Name:       r.FormValue("name"),
+        Value:      r.FormValue("value"),
         Comment:    r.FormValue("comment"),
-        Date:   time.Now(),
+        Date:       time.Now(),
     }
     key := datastore.NewKey(c, "Entry", "default_entry", 0, nil)
     key = datastore.NewIncompleteKey(c, "Entry", key)
