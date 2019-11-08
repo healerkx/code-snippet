9aa014ca400a2f8bfad43358ddbc06ec3fac8e2d
yuzhongmin <yuzhongmin@guazi.com>
2019-11-08 21:39:56 +0800

    Working day

--- lang/python/schedule/sched.py
+++ lang/python/schedule/sched.py
@@ -4,6 +4,7 @@ from functools import reduce
 from pyquery import PyQuery as pq
 from optparse import OptionParser
 
+Date_Format = "%Y-%m-%d"
 
 class Objective:
     def __init__(self):
@@ -27,7 +28,7 @@ class Project:
         return self.stages[-1]
 
     def all_assignees(self):
-        return set(reduce(lambda y, x: y + list(x.all_assignees()), self.stages, []))
+        return sorted(set(reduce(lambda y, x: y + list(x.all_assignees()), self.stages, [])))
 
 Stage_Design    = 1
 Stage_Develop   = 2
@@ -44,8 +45,7 @@ Stages = {
 
 def parse_stage(stage):
     for s, n in Stages.items():
-        if n[0] == stage:
-            return s
+        if n[0] == stage: return s
     return 0
 
 class Stage:
@@ -58,7 +58,7 @@ class Stage:
         self.tasks.append(task)
 
     def all_assignees(self):
-        return set(reduce(lambda y, x: y + x.assignees, self.tasks, []))
+        return sorted(set(reduce(lambda y, x: y + x.assignees, self.tasks, [])))
 
 class Task:
     def __init__(self, task_name, begin_date, end_date):
@@ -75,13 +75,12 @@ def get_earliest_date(o):
     for p in o.projects:
         for s in p.stages:
             for t in s.tasks:
-                if date > t.begin_date:
-                    date = t.begin_date
+                if date > t.begin_date: date = t.begin_date
     return date
 
 
 def regular_date(date):
-    return dateutil.parser.parse(date).strftime("%Y-%m-%d")
+    return dateutil.parser.parse(date).strftime(Date_Format)
 
 def parse_date(date):
     date = date.strip()
@@ -100,7 +99,7 @@ def parse_time_range(time_range_part):
     else: return [time_range_part, time_range_part]
 
 def parse_line(line):
-    r = re.findall(r"(#+)\s*(\w*)\s*(\[[\w~-]*\])?\s*(@.*)?", line)
+    r = re.findall(r"(#+)\s*([\w~\-_+]*)\s*(\[[\w~-]*\])?\s*(@.*)?", line)
 
     if not r or len(r) == 0:
         return None
@@ -125,6 +124,7 @@ def parse_sched_file(sched_file):
             line = line.strip()
             if not line: continue
             res = parse_line(line)
+            if not res: continue
             if res[0] == 1:
                 current_project = Project(res[1])
                 o.add_project(current_project)
@@ -139,8 +139,8 @@ def parse_sched_file(sched_file):
                 for assignee in res[3]:
                     task.add_assignee(assignee)
                 current_stage.add_task(task)
-
         return o
+    return None
 
 def my_tasks(project, assignee):
     tasks = []
@@ -158,19 +158,33 @@ def visit_objective(o, func=None):
             for t in s.tasks:
                 print(f"{t.task_name}: {t.begin_date}~{t.end_date}, {t.assignees}")
 
-def dates(date_begin=None, date_count=14):
+def is_working_day(date):
+    if isinstance(date, str): weekday = dateutil.parser.parse(date).weekday()
+    else: weekday = date.weekday()
+    return weekday != 5 and weekday != 6
+
+def dates(date_begin=None, date_count=14, only_working_dates=True):
     if not date_begin:
         date_begin = datetime.datetime.now()
     elif isinstance(date_begin, str):
         date_begin = dateutil.parser.parse(date_begin)
-    f = lambda x: (date_begin+datetime.timedelta(days=x)).strftime("%Y-%m-%d")
-    return [f(i) for i in range(0, date_count)]
+    f = lambda x: date_begin + datetime.timedelta(days=x)
+    dates_list = [f(i) for i in range(0, date_count)]
+    if only_working_dates:
+        return list(filter(lambda x: is_working_day(x), dates_list))
+    return dates_list
 
-def dates_diff(begin, end):
-    a = dateutil.parser.parse(begin)
-    b = dateutil.parser.parse(end)
-    return (b - a).days
+def dates_diff(begin, end, only_working_dates=True):
+    b, e = dateutil.parser.parse(begin), dateutil.parser.parse(end)
+    if only_working_dates:
+        count = 0
+        while b != e:
+            if is_working_day(b): count += 1
+            b += datetime.timedelta(1)
+        return count
+    return (e - b).days
 
+def set_bgcolor(elem, color): elem.attr.style = f"background-color: {color}"
 
 def prepare_bootstrap_page():
     page = pq("<html></html")
@@ -179,21 +193,29 @@ def prepare_bootstrap_page():
     page.append(body)
     return (page, body)
 
-def create_tab(body, date_begin, date_count):
+def create_tab(body, date_begin, date_count, only_working_dates):
     tab = pq("<table>")
     tab.add_class("table table-bordered table-hover table-striped")
-    thead = create_tab_header(tab, date_begin, date_count)
+    thead = create_tab_header(tab, date_begin, date_count, only_working_dates)
     tbody = pq("<tbody>")
     tab.append(tbody)
     body.append(tab)
     return tab, thead, tbody
 
-def create_tab_header(tab, date_begin, date_count):
+def create_tab_header(tab, date_begin, date_count, only_working_dates):
     thead, tr = pq("<thead>"), pq("<tr>")
     tr.append(pq("<td>项目</td>"))
     tr.append(pq("<td style='min-width:5em'>RD</td>"))
+    today = datetime.datetime.today().strftime(Date_Format)
     for date in dates(date_begin, date_count):
-        tr.append(pq(f"<td>{date}</td>"))
+        if only_working_dates and not is_working_day(date):
+            continue
+        display = date.strftime(Date_Format)[5:].replace("-", "/")
+        if only_working_dates and is_working_day(date):
+            if date.weekday() == 0: display += " (Mon)"
+        date_td = pq(f"<td><span>{display}</span></td>")
+        if date.strftime(Date_Format) == today: set_bgcolor(date_td, "yellow")
+        tr.append(date_td)
     thead.append(tr)
     tab.append(thead)
     return thead
@@ -201,15 +223,17 @@ def create_tab_header(tab, date_begin, date_count):
 def stage_color(stage):
     return Stages[stage][1]
 
-def gen_project_view(o, date_begin, date_count):
+def gen_project_view(o, date_begin, date_count, only_working_dates):
     page, body = prepare_bootstrap_page()
-    _tab, _thead, tbody = create_tab(body, date_begin, date_count)
+    _tab, _thead, tbody = create_tab(body, date_begin, date_count, only_working_dates)
     
-    dates_list = dates(date_begin, date_count)
+    dates_list = dates(date_begin, date_count, only_working_dates)
+    dates_list = list(map(lambda x: x.strftime(Date_Format), dates_list))
     for p in o.projects:
         has_project_name = False
         for assignee in p.all_assignees():
             tr = pq(f"<tr>")
+            tbody.append(tr)
             if not has_project_name:
                 proj_td = pq(f"<td>{p.project_name}</td>")
                 proj_td.attr.rowspan = str(len(p.all_assignees()))
@@ -227,11 +251,12 @@ def gen_project_view(o, date_begin, date_count):
                 hit_some_task_begin_date = False
                 diff = 1
                 for (stage, task) in tasks:
+                    # TODO: 如果date_begin大于某个task的begin, 那么整个task就不会显示了
                     if task.begin_date == dates_list[i]:
                         hit_some_task_begin_date = True
                         date_td = pq(f"<td>")
 
-                        diff = dates_diff(task.begin_date, task.end_date) + 1
+                        diff = dates_diff(task.begin_date, task.end_date, only_working_dates) + 1
                         date_td.append(pq(f"<span>{task.task_name}</span>"))
                         date_td.attr.colspan = str(diff)
                         color = stage_color(stage.stage)
@@ -241,20 +266,17 @@ def gen_project_view(o, date_begin, date_count):
                 if not hit_some_task_begin_date:
                     date_td = pq(f"<td>")
                     tr.append(date_td)
-                    i += 1
-                else:
-                    i += diff
 
-            tbody.append(tr)
+                i += diff
+
     return page
 
 if __name__ == '__main__':
-
     parser = OptionParser()
-
     parser.add_option("-f", "--file", action="store", dest="file", help="Provide schedule filename")
     parser.add_option("-b", "--date-begin", action="store", dest="date_begin", help="Provide view begin date")
     parser.add_option("-c", "--date-count", action="store", dest="date_count", help="Provide view date count", default=30)
+    parser.add_option("-w", "--only-working-dates", action="store", dest="only_working_dates", help="Provide working dates setting", default=True)
 
     options, args = parser.parse_args()
     filename = options.file
@@ -265,8 +287,8 @@ if __name__ == '__main__':
     date_begin = options.date_begin
     if not date_begin:
         date_begin = get_earliest_date(o)
-        
-    page = gen_project_view(o, date_begin, options.date_count)
+
+    page = gen_project_view(o, date_begin, int(options.date_count), options.only_working_dates)
 
     with open(os.path.join(os.path.dirname(filename), "schedule.html"), "w") as file:
         file.write(str(page))
